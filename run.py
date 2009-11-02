6e50e363bf6549c5ffa6ac3cc2226209fad221f7
kratorius <giuliani.v@gmail.com>
2009-11-02 23:22:39 +0100

    Basic running

--- run.py
+++ run.py
@@ -7,7 +7,36 @@ import sys
 import getopt
 
 def argcheck(opts):
-    print opts
+    class Options:
+        "Default arguments"
+        client = False
+        server = False
+        listen_to = "127.0.0.1"
+        server_addr = "127.0.0.1"
+
+        def check(self):
+            client = self.client
+            server = self.server
+            listen_to = self.listen_to
+            server_addr = self.server_addr
+
+            if client and server or not (client or server):
+                return (False, "Cannot be both client and server simultaneosly")
+            # TODO: check for listen_to/server_addr validity
+            # elif ...
+            return (True, "ok")
+
+    o = Options()
+    for name, value in opts:
+        if name == "--client":
+            o.client = True
+        elif name == "--server":
+            o.server = True
+        elif name == "--listen-to":
+            o.listen_to = value
+        elif name == "--server-addr":
+            o.server_addr = value
+    return o
 
 def main(argv):
     try:
@@ -17,13 +46,25 @@ def main(argv):
         return True
 
     options = argcheck(opts)
+    check = options.check()
+    if not check[0]:
+        usage(argv[0], check[1])
+        return True
+
+    if options.client:
+        from lightmon.client import runner
+        runner.run()
+    elif options.server:
+        from lightmon.server import runner
+        runner.run()
 
     return False
 
-def usage(name):
+def usage(name, errstring=None):
     """
     Print usage instructions
     """
+    if errstring: print errstring
     print """%s <options>
     --client                run in client mode
     --server                run in server mode
