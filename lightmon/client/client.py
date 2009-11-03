f06490cae2ca4f9f8b51fe920148c593d63fc7dd
Ivan Giuliani <giuliani.v@gmail.com>
2009-11-04 00:18:30 +0100

    Narrowed down some logic about job addition

--- lightmon/client/client.py
+++ lightmon/client/client.py
@@ -8,6 +8,7 @@ import sched
 import time
 
 from lightmon import config
+from lightmon import jobs
 
 class Client(object):
     """
@@ -19,12 +20,15 @@ class Client(object):
         self.scheduler = sched.scheduler(timefunc, delayfunc)
         self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
 
+        self.jobs = {}
+
     def controller(self):
         """
         Control if there are stale threads that needs to be killed
         """
         # reschedule ourselves every SELF_CHECK_EVERY seconds
-        self.checkJobs()
+        # self.checkJobs()
+        print "not yet: controller"
         self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
 
     def checkJobs(self):
@@ -34,9 +38,25 @@ class Client(object):
         """
         raise NotImplementedError
 
-    def addJob(self):
+    def addJob(self, job):
         "Add a new check job"
-        raise NotImplementedError
+        assert isinstance(job, jobs.Job)
+
+        # XXX: the name might not be the best thing as an index due to collisions
+        self.jobs[job.name] = job
+        self.scheduler.enter(job.delay, 1, self.runJob, (job.name, ))
+
+    def runJob(self, name):
+        """
+        Runs a job. This method gets called by the scheduler and might be
+        killed if it takes too much time.
+        """
+        print "[run %s]" % name
+        # TODO: run the real job
+
+        # reschedule the job if requested
+        if self.jobs.haskey(name) and self.jobs[name].repeat:
+            self.scheduler.enter(self.jobs[name], 1, self.runJob, (name, ))
 
     def run(self):
         "Run the client"
---
+++ lightmon/jobs.py
@@ -0,0 +1,10 @@
+class Job(object):
+    "The base class for jobs"
+    def __init__(self, name, delay=5, repeat=False, *args, **kwargs):
+        self.name = name
+        self.delay = delay
+        self.repeat = repeat
+
+    def run(self):
+        raise NotImplementedError(
+                "run() method not implemented for %s check" % self.name)
