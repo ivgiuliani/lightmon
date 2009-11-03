89a61ba97b2f1e6c524efd69bf607dcb0eed97dc
kratorius <giuliani.v@gmail.com>
2009-11-03 17:06:32 +0100

    Added clarification comment above the client runner

--- lightmon/client/runner.py
+++ lightmon/client/runner.py
@@ -1,2 +1,8 @@
+"""
+The client runs on the "timeline", that is a series of time-scheduled events.
+Indeed, service checking classes are run on fixed time intervals as well as the
+reports back to the server.
+"""
+
 def run():
     print "client"
