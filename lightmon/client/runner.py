683a0e7ce5a919570b01f05f3c8a1638403f69b3
Ivan Giuliani <giuliani.v@gmail.com>
2009-11-03 23:45:49 +0100

    Split client class from the runner

--- lightmon/client/runner.py
+++ lightmon/client/runner.py
@@ -1,43 +1,14 @@
-"""
-The client runs on the "timeline", that is a series of time-scheduled events.
-Indeed, service checking classes are run on fixed time intervals as well as the
-reports back to the server.
-"""
-
-import sched
 import sys
-import time
 
-from lightmon import config
-
-class Client(object):
-    """
-    The main client class. Here we handle the time scheduled events
-    (included the lightmon process itself)
-    """
-    def __init__(self):
-        # schedule the lightmon process as every ``SELF_CHECK_EVERY``
-        self.scheduler = sched.scheduler(time.time, time.sleep)
-        self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
-
-    def controller(self):
-        """
-        Control if there are stale threads that needs to be killed
-        """
-        print 'not yet: controller'
-        # reschedule ourselves every SELF_CHECK_EVERY seconds
-        self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
-
-    def run(self):
-        self.scheduler.run()
+from lightmon.client import client
 
 def run():
     """
     Start the client timeline
     """
-    client = Client()
+    cl = client.Client()
 
     try:
-        client.run()
+        cl.run()
     except KeyboardInterrupt:
         sys.stderr.write("goodbye Captain\n")
