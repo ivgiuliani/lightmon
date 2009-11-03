0350f6b81ef1ecb1ae7b22073ab168806e22a0be
Ivan Giuliani <giuliani.v@gmail.com>
2009-11-03 23:57:43 +0100

    Put down some more logic

--- lightmon/client/client.py
+++ lightmon/client/client.py
@@ -14,19 +14,31 @@ class Client(object):
     The main client class. Here we handle the time scheduled events
     (included the lightmon process itself)
     """
-    def __init__(self):
-        # schedule the lightmon process as every ``SELF_CHECK_EVERY``
-        self.scheduler = sched.scheduler(time.time, time.sleep)
+    def __init__(self, timefunc=time.time, delayfunc=time.sleep):
+        # schedule the lightmon controller process as every ``SELF_CHECK_EVERY``
+        self.scheduler = sched.scheduler(timefunc, delayfunc)
         self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
 
     def controller(self):
         """
         Control if there are stale threads that needs to be killed
         """
-        print 'not yet: controller'
         # reschedule ourselves every SELF_CHECK_EVERY seconds
+        self.checkJobs()
         self.scheduler.enter(config.SELF_CHECK_EVERY, 1, self.controller, ())
 
+    def checkJobs(self):
+        """
+        Check the current status of the check jobs, if the same job is in
+        execution twice (or more) kill the older job and report a failure
+        """
+        raise NotImplementedError
+
+    def addJob(self):
+        "Add a new check job"
+        raise NotImplementedError
+
     def run(self):
+        "Run the client"
         self.scheduler.run()
 
