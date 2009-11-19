9bdf5e810114f9b673f4741714316d4369670769
Ivan Giuliani <giuliani.v@gmail.com>
2009-11-19 18:18:47 +0100

    Added a slightly better argument evaluator (it's still one of the worst code pieces ever though)

--- lightmon/config.py
+++ lightmon/config.py
@@ -3,6 +3,11 @@ Default settings for lightmon
 """
 import logging
 
+# these two variables are True according to in which modes
+# lightmon is running
+CLIENT = False
+SERVER = False
+
 # self check the lightmon status every SELF_CHECK_EVERY seconds
 SELF_CHECK_EVERY = 1
 
@@ -14,6 +19,12 @@ LOGGING_FILE = "lightmon.log"
 
 # maximum time (in seconds) that threads can use to execute
 MAX_CHECK_EXECUTION_TIME = 15
+
+# server listening address (only server)
+LISTEN_ADDR = "127.0.0.1"
+
+# server address (only client)
+SERVER_ADDR = "127.0.0.1"
 
 # the job list
 JOBS = []
--- run.py
+++ run.py
@@ -9,49 +9,42 @@ import logging
 
 from lightmon import config
 
-def argcheck(opts):
-    class Options:
-        "Default arguments"
-        client = False
-        server = False
-        listen_to = "127.0.0.1"
-        server_addr = "127.0.0.1"
-        logging_level = logging.INFO
+def args_evaluation(opts):
+    """
+    Checks that the arguments are valid
+    """
 
-        def check(self):
-            client = self.client
-            server = self.server
-            listen_to = self.listen_to
-            server_addr = self.server_addr
-            logging_level = self.logging_level
-
-            if client and server or not (client or server):
-                return (False, "Cannot be both client and server simultaneosly")
-            # TODO: check for listen_to/server_addr validity
-            # elif ...
-            return (True, "ok")
-
-    o = Options()
     for name, value in opts:
         if name == "--client":
-            o.client = True
+            config.CLIENT = True 
         elif name == "--server":
-            o.server = True
+            config.SERVER = True
         elif name == "--listen-to":
-            o.listen_to = value
+            config.LISTEN_ADDR = value
         elif name == "--server-addr":
-            o.server_addr = value
+            config.SERVER_ADDR = value
         elif name == "--logging-level":
             values = {
                 'DEBUG': logging.DEBUG,
                 'INFO': logging.INFO,
             }
-            o.logging_level = values[value]
-    return o
+            config.LOGGIN_LEVEL = values[value]
+
+    # check arguments validity
+    if config.CLIENT and config.SERVER:
+        return False
+
+    return True
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv[1:], "", ["client", "server", "listen-to=", "server-addr=", "logging-level="])
+        opts, args = getopt.getopt(argv[1:], "", [
+            "client",
+            "server",
+            "listen-to=",
+            "server-addr=",
+            "logging-level=",
+            "settings="])
     except getopt.GetoptError, err:
         usage(argv[0])
         return True
@@ -60,17 +53,14 @@ def main(argv):
         usage(argv[0])
         return True
 
-    options = argcheck(opts)
-    check = options.check()
-    if not check[0]:
+    if not args_evaluation(opts):
         usage(argv[0], check[1])
         return True
 
-    config.LOGGING_LEVEL = options.logging_level
-    if options.client:
+    if config.CLIENT:
         from lightmon.client import runner
         runner.run()
-    elif options.server:
+    elif config.SERVER:
         from lightmon.server import runner
         runner.run()
 
@@ -81,13 +71,14 @@ def usage(name, errstring=None):
     Print usage instructions
     """
     if errstring: print errstring
-    print """%s <options>
+    print("""%s <options>
     --client                run in client mode
     --server                run in server mode
+    --config <config>       use <config> module as config
     --listen-to <port>      listen to the port <port> (only for server mode)
     --server-addr <server>  set <server> as the server to connect (only for client mode)
     --logging-level <level> set the logging level as <level> (can be INFO or DEBUG)
-    """ % name
+    """ % name)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
